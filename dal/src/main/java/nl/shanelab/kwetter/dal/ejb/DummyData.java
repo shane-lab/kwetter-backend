aeb8e02c409aaf301ad44a78b8debbfdaae68f15
Shane <shane.vandenbogaard@student.fontys.nl>
2018-03-10 15:01:10 +0100

    Applied fix to dummydata class for lazyloading recursive fiels on domain entities

--- dal/src/main/java/nl/shanelab/kwetter/dal/ejb/DummyData.java
+++ dal/src/main/java/nl/shanelab/kwetter/dal/ejb/DummyData.java
@@ -49,61 +49,84 @@ public class DummyData {
         User userBob = new User("Bob", "bob", Role.USER);
 
         users.put(++usersIndex, userShane);
+        userShane.setId(usersIndex);
         users.put(++usersIndex, userAlice);
+        userAlice.setId(usersIndex);
         users.put(++usersIndex, userBob);
+        userBob.setId(usersIndex);
+
+        assert userShane.getId() == 0;
+        assert userAlice.getId() == 1;
+        assert userBob.getId() == 2;
 
         // initialize HashTags
-//        long hashTagsIndex = -1;
-//
-//        HashTag hashTagFhict = new HashTag("FHICT");
-//        HashTag hashTagKwetter = new HashTag("kwetter");
-//
-//        hashTags.put(++hashTagsIndex, hashTagFhict);
-//        hashTags.put(++hashTagsIndex, hashTagKwetter);
-//
-//        // initialize Kweets
-//        long kweetsIndex = -1;
-//
-//        // kweets for userShane
-//        Kweet kweetShane = new Kweet("Message by Shane", userShane);
-//        Kweet kweetShane1 = new Kweet("Message with hashtags by Shane #FHICT #kwetter", userShane);
-//
-//        // kweets for userAlice
-//        Kweet kweetAlice = new Kweet("Message by Alice", userAlice);
-//
-//        // kweets for userBob
-//        Kweet kweetBob = new Kweet("Message by Bob", userBob);
-//        Kweet kweetBob1 = new Kweet("Message by Bob, in reply to @Shane", userBob);
-//
-//        kweets.put(++kweetsIndex, kweetShane);
-//        kweets.put(++kweetsIndex, kweetShane1);
-//        kweets.put(++kweetsIndex, kweetAlice);
-//        kweets.put(++kweetsIndex, kweetBob);
-//        kweets.put(++kweetsIndex, kweetBob1);
-//
-//        // set hashtags
-//        setKweetHashTag(kweetShane1, hashTagFhict);
-//        setKweetHashTag(kweetShane1, hashTagKwetter);
-//
-//        // set mentions
-//        setKweetMention(kweetBob1, userShane);
-//
-//        // set favorited
-//        setKweetFavouritedBy(kweetShane, userAlice);
-//        setKweetFavouritedBy(kweetShane1, userAlice);
-//        setKweetFavouritedBy(kweetShane1, userBob);
-//
-//        setKweetFavouritedBy(kweetAlice, userShane);
-//
-//        // set following
-//        createFollow(userShane, userAlice);
-//        createFollow(userShane, userBob);
-//
-//        createFollow(userAlice, userShane);
-//        createFollow(userAlice, userBob);
-//
-//        createFollow(userBob, userShane);
-//        createFollow(userBob, userAlice);
+        long hashTagsIndex = -1;
+
+        HashTag hashTagFhict = new HashTag("FHICT");
+        HashTag hashTagKwetter = new HashTag("kwetter");
+
+        hashTags.put(++hashTagsIndex, hashTagFhict);
+        hashTagFhict.setId(hashTagsIndex);
+        hashTags.put(++hashTagsIndex, hashTagKwetter);
+        hashTagKwetter.setId(hashTagsIndex);
+
+        assert hashTagFhict.getId() == 0;
+        assert hashTagKwetter.getId() == 1;
+
+        // initialize Kweets
+        long kweetsIndex = -1;
+
+        // kweets for userShane
+        Kweet kweetShane = new Kweet("Message by Shane", userShane);
+        Kweet kweetShane1 = new Kweet("Message with hashtags by Shane #FHICT #kwetter", userShane);
+
+        // kweets for userAlice
+        Kweet kweetAlice = new Kweet("Message by Alice", userAlice);
+
+        // kweets for userBob
+        Kweet kweetBob = new Kweet("Message by Bob", userBob);
+        Kweet kweetBob1 = new Kweet("Message by Bob, in reply to @Shane", userBob);
+
+        kweets.put(++kweetsIndex, kweetShane);
+        kweetShane.setId(kweetsIndex);
+        kweets.put(++kweetsIndex, kweetShane1);
+        kweetShane1.setId(kweetsIndex);
+        kweets.put(++kweetsIndex, kweetAlice);
+        kweetAlice.setId(kweetsIndex);
+        kweets.put(++kweetsIndex, kweetBob);
+        kweetBob.setId(kweetsIndex);
+        kweets.put(++kweetsIndex, kweetBob1);
+        kweetBob1.setId(kweetsIndex);
+
+        assert kweetShane.getId() == 0;
+        assert kweetShane1.getId() == 1;
+        assert kweetAlice.getId() == 2;
+        assert kweetBob.getId() == 3;
+        assert kweetBob1.getId() == 4;
+
+        // set hashtags
+        setKweetHashTag(kweetShane1, hashTagFhict);
+        setKweetHashTag(kweetShane1, hashTagKwetter);
+
+        // set mentions
+        setKweetMention(kweetBob1, userShane);
+
+        // set favorited
+        setKweetFavouritedBy(kweetShane, userAlice);
+        setKweetFavouritedBy(kweetShane1, userAlice);
+        setKweetFavouritedBy(kweetShane1, userBob);
+
+        setKweetFavouritedBy(kweetAlice, userShane);
+
+        // set following
+        createFollow(userShane, userAlice);
+        createFollow(userShane, userBob);
+
+        createFollow(userAlice, userShane);
+        createFollow(userAlice, userBob);
+
+        createFollow(userBob, userShane);
+        createFollow(userBob, userAlice);
     }
 
     /**
@@ -126,8 +149,18 @@ public class DummyData {
             following = new HashSet<>();
         }
 
-        followers.add(b);
-        following.add(a);
+        // omit circular reference recursion (lazy LAZY-implementation)
+        User newUserA = new User(a.getUsername(), a.getPassword(), a.getRole());
+        newUserA.setId(a.getId());
+        newUserA.setBio(a.getBio());
+        newUserA.setKweets(a.getKweets());
+        User newUserB = new User(b.getUsername(), b.getPassword(), b.getRole());
+        newUserB.setId(b.getId());
+        newUserB.setBio(b.getBio());
+        newUserB.setKweets(b.getKweets());
+
+        followers.add(newUserB);
+        following.add(newUserA);
 
         a.setFollowers(followers);
         b.setFollowing(following);
@@ -152,7 +185,7 @@ public class DummyData {
 
         kweet.setHashTags(hashTags);
 
-        kweets.put(kweet.getId(), kweet);
+        kweets.replace(kweet.getId(), kweet);
     }
 
     /**
@@ -171,7 +204,7 @@ public class DummyData {
 
         kweet.setMentions(mentions);
 
-        kweets.put(kweet.getId(), kweet);
+        kweets.replace(kweet.getId(), kweet);
     }
 
     /**
@@ -190,13 +223,26 @@ public class DummyData {
             favourited = new HashSet<>();
         }
 
-        favouritedBy.add(user);
-        favourited.add(kweet);
+        // omit circular reference recursion (lazy LAZY-implementation)
+        Kweet newKweet = new Kweet(kweet.getMessage(), kweet.getAuthor());
+        newKweet.setId(kweet.getId());
+        newKweet.setCreatedAt(kweet.getCreatedAt());
+        newKweet.setHashTags(kweet.getHashTags());
+        newKweet.setMentions(kweet.getMentions());
+        User newUser = new User(user.getUsername(), user.getPassword(), user.getRole());
+        newUser.setId(user.getId());
+        newUser.setBio(user.getBio());
+        newUser.setKweets(user.getKweets());
+        newUser.setFollowing(user.getFollowing());
+        newUser.setFollowers(user.getFollowers());
+
+        favouritedBy.add(newUser);
+        favourited.add(newKweet);
 
         kweet.setFavoritedBy(favouritedBy);
         user.setFavoriteKweets(favourited);
 
-        kweets.put(kweet.getId(), kweet);
-        users.put(user.getId(), user);
+        kweets.replace(kweet.getId(), kweet);
+        users.replace(user.getId(), user);
     }
 }
