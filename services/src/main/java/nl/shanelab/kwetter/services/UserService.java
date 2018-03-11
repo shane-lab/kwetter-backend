ed0a297f156e8210105e5346eeec08a1a9d7f226
Shane <shane.vandenbogaard@student.fontys.nl>
2018-03-11 01:31:25 +0100

    Implemented UserService and implemenation bean

--- services/src/main/java/nl/shanelab/kwetter/services/UserService.java
+++ services/src/main/java/nl/shanelab/kwetter/services/UserService.java
@@ -1,4 +1,23 @@
 package nl.shanelab.kwetter.services;
 
+import nl.shanelab.kwetter.dal.domain.Role;
+import nl.shanelab.kwetter.dal.domain.User;
+
+import java.util.Collection;
+
 public interface UserService {
+
+    User register(String name, String password);
+
+    User register(String name, String password, Role role);
+
+    User edit(User user);
+
+    void remove(User user);
+
+    User getById(long id);
+
+    User getByUserName(String name);
+
+    Collection<User> getAllUsers();
 }
--- services/src/main/java/nl/shanelab/kwetter/services/impl/UserServiceImpl.java
+++ services/src/main/java/nl/shanelab/kwetter/services/impl/UserServiceImpl.java
@@ -1,14 +1,48 @@
 package nl.shanelab.kwetter.services.impl;
 
 import nl.shanelab.kwetter.dal.dao.UserDao;
+import nl.shanelab.kwetter.dal.domain.Role;
+import nl.shanelab.kwetter.dal.domain.User;
 import nl.shanelab.kwetter.dal.qualifiers.InMemoryDao;
 import nl.shanelab.kwetter.services.UserService;
 
+import javax.ejb.Stateless;
 import javax.inject.Inject;
+import java.util.Collection;
 
+@Stateless
 public class UserServiceImpl implements UserService {
     
     @InMemoryDao
     @Inject
     UserDao userDao;
+
+    public User register(String name, String password) {
+        return this.register(name, password, Role.USER);
+    }
+
+    public User register(String name, String password, Role role) {
+//        User user = getByUserName(name); throw (custom?) error, user already exist
+        return userDao.create(new User(name, password, role));
+    }
+
+    public User edit(User user) {
+        return userDao.edit(user);
+    }
+
+    public void remove(User user) {
+        userDao.remove(user);
+    }
+
+    public User getById(long id) {
+        return userDao.find(id);
+    }
+
+    public User getByUserName(String name) {
+        return userDao.getByUsername(name);
+    }
+
+    public Collection<User> getAllUsers() {
+        return userDao.findAll();
+    }
 }
